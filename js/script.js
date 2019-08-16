e38e3bf8e4c1936a9cae332f87ab7505237f60d1
Niko <niko@mlh.io>
2019-08-15 21:16:36 -0400

    Updates

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const checkbox = document.querySelector("input[type='checkbox']");
+
+checkbox.addEventListener("click", e => {
+  const checked = e.target.checked;
+  const body = document.querySelector('body');
+  const internet = document.querySelector('.internet');
+  const arr = document.querySelectorAll('.list i');
+  const marquees = document.querySelectorAll('marquee');
+
+  body.classList.toggle('bg-near-black', checked);
+  body.classList.toggle('washed-yellow', checked);
+  internet.classList.toggle('glitter', checked);
+
+  arr.forEach(el => el.classList.toggle('glitter', checked) );
+  marquees.forEach(el => el.classList.toggle('o-0', !checked) );
+});
---
+++ precache-manifest.eb96ec4b4539812aab34067fa12c65d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640bb456f9f59b453d5c441dc86c2127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acc80fa6b1ea9da39371",
+    "url": "/static/css/main.8ab37214.chunk.css"
+  },
+  {
+    "revision": "2b10920364dceae7926f",
+    "url": "/static/js/2.4053a06b.chunk.js"
+  },
+  {
+    "revision": "acc80fa6b1ea9da39371",
+    "url": "/static/js/main.b756e68a.chunk.js"
+  },
+  {
+    "revision": "57087445b8c3d9b59894",
+    "url": "/static/js/runtime~main.c2f40307.js"
+  },
+  {
+    "revision": "270fbd5465339c75ddfa715124d2f1be",
+    "url": "/static/media/glitter.270fbd54.gif"
+  }
+]);
