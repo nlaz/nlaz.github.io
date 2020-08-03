1b9102e9f2a49032ec8f0e4550325f7eabca3ed8
Niko <niko@mlh.io>
2020-08-03 12:29:33 -0400

    Update page structure, remove complexity

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,119 @@
 import React, { useState } from "react";
+import cx from "classnames";
+
 import Meta from "../components/Meta";
-import Navbar from "../components/Navbar";
+import Anchor from "../components/Anchor";
 import ProjectItem from "../components/home/ProjectItem";
 import TeamItem from "../components/home/TeamItem";
 
-import projectData from "../data/projectSection.json";
-import teamData from "../data/teamSection.json";
+import work from "../data/work.json";
+import education from "../data/education.json";
 
-const HomePage = ({ projects, teams }) => {
+const RowItem = ({ logo, title, subtitle, link, period, description }) => (
+  <div className="row-item flex items-center pv2 ph3 bb b--light-gray">
+    <img
+      className="bg-light-gray  br2 mr3 ml2 mv1"
+      style={{ height: "48px", width: "48px" }}
+      src={logo}
+    />
+    <div className="flex-auto">
+      <div className="f6 fw5 purple" style={{ marginBottom: "1px" }}>
+        {title}
+      </div>
+      <div className="f6 fw4 light-gray">{subtitle}</div>
+    </div>
+    <div className="w-20 f6 fw5 light-gray mr2">{period}</div>
+  </div>
+);
+
+const HomePage = ({ projects, work, education }) => {
   const [showTeams, setShowTeams] = useState(true);
   const [showProjects, setShowProjects] = useState(true);
+  const [showWork, setShowWork] = useState(false);
+  const filteredWork = showWork ? work : work.slice(0, 3);
   return (
-    <div className="helvetica relative">
+    <div className="helvetica relative bg-near-white near-black pv5">
       <Meta
         title="Niko Lazaris"
         description="I build things for the internet. Product Manager at Major League Hacking. Previously Meetup, hackNY, and HubSpot."
       />
-      <Navbar />
-      <div className="mw9 center w-100 mb3 mb6-ns pv4 ph3 ph5-m ph5-l">
-        <div className="ph2 center text-center measure">
-          <h1 className="f2 f1-ns ma0">Niko Lazaris</h1>
-          <h2 className="f3 mt0 normal">
-            building things on the <span className="internet">internet</span>.
-          </h2>
+      <div className="mw8 center mb5">
+        <h1 className="f1 dark-gray fw4 mv0">Niko Lazaris</h1>
+        <p className="mt1 light-gray mb5" style={{ fontSize: "18px" }}>
+          Building things for the internet. Senior Product Manager at Major
+          League Hacking.
+        </p>
+      </div>
+      <div className="mw8 center mb5">
+        <h3 className="prime purple f4 fw4">Projects</h3>
+      </div>
+      <div className={cx("mw8 center fw4", { mb5: showWork })}>
+        <h3 className="prime purple f4 fw4 pb1">Work Experience</h3>
+        <div className="mt3 br3 bg-white shadow">
+          {filteredWork.map((item, key) => (
+            <RowItem
+              key={key}
+              logo={item.logo}
+              title={item.role}
+              subtitle={item.company}
+              description={item.description}
+              period={item.period}
+            />
+          ))}
+        </div>
+        {!showWork && (
+          <div className="mt3 flex justify-center">
+            <button
+              onClick={() => setShowWork(true)}
+              className="ba b--moon-gray purple f6 fw5 br2 ph3 pv1 pointer"
+            >
+              Show More
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="mw8 center mb5">
+        <h3 className="prime purple f4 fw4">Education</h3>
+        <div className="mt3 br3 bg-white shadow">
+          {education.map((item, key) => (
+            <RowItem
+              key={key}
+              logo={item.logo}
+              title={item.degree}
+              subtitle={item.school}
+              period={item.period}
+            />
+          ))}
         </div>
       </div>
-
-      <div className="mw9 mb5 center">
-        <h3
-          className="ph3 ph2-m ph4-l mb4 courier normal pointer"
-          onClick={() => setShowTeams(!showTeams)}
-        >
-          {showTeams ? "-" : "+"} Teams
-        </h3>
-        {showTeams && (
-          <div className="flex flex-wrap">
-            {teams.map((team, index) => (
-              <div className="w-50-m w-25-l ph3 ph2-m ph4-l mb5" key={index}>
-                <TeamItem team={team} />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-
-      <div className="mw9 mb6 center">
-        <h3
-          className="ph3 ph2-m ph4-l mb4 courier normal pointer"
-          onClick={() => setShowProjects(!showProjects)}
-        >
-          {showProjects ? "-" : "+"} Projects
-        </h3>
-        {showProjects && (
-          <div className="flex flex-wrap">
-            {projects.map((project, index) => (
-              <div className="w-50-m w-25-l ph3 ph2-m ph4-l mb5" key={index}>
-                <ProjectItem project={project} />
-              </div>
-            ))}
-          </div>
-        )}
+      <div className="mw8 center mb5">
+        <h3 className="prime purple f4 fw4">Contact</h3>
+        <p className="mt2">
+          If you are intested in working together, send me a message or{" "}
+          <Anchor href="mailto:nikolazaris@gmail.com" className="color-inherit">
+            email me
+          </Anchor>
+          .
+        </p>
+        <div className="flex justify-center center mt4">
+          <Anchor className="color-inherit" href="https://github.com/nlaz">
+            GitHub
+          </Anchor>
+          <span className="mh2"> · </span>
+          <Anchor
+            className="color-inherit"
+            href="https://www.linkedin.com/in/niko-lazaris-88946186/"
+          >
+            LinkedIn
+          </Anchor>
+          <span className="mh2"> · </span>
+          <Anchor
+            className="color-inherit"
+            href="https://twitter.com/nikolazaris"
+          >
+            Twitter
+          </Anchor>
+        </div>
       </div>
     </div>
   );
@@ -67,8 +121,10 @@ const HomePage = ({ projects, teams }) => {
 
 HomePage.getInitialProps = async () => {
   return {
-    projects: projectData.projects.map(i => i.fields).filter(i => i),
-    teams: teamData.teams.map(i => i.fields).filter(i => i)
+    projects: [],
+    work: work,
+    communities: [],
+    education: education,
   };
 };
 
