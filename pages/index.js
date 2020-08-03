1c7f8e398563be6bea06cf157e5e4b82238db0c5
Niko <niko@mlh.io>
2020-08-03 13:04:27 -0400

    Add communities section

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import TeamItem from "../components/home/TeamItem";
 
 import work from "../data/work.json";
 import education from "../data/education.json";
+import communities from "../data/communities.json";
 
 const RowItem = ({ logo, title, subtitle, link, period, description }) => (
   <div className="row-item flex items-center pv2 ph3 bb b--light-gray">
@@ -22,15 +23,22 @@ const RowItem = ({ logo, title, subtitle, link, period, description }) => (
       </div>
       <div className="f6 fw4 light-gray">{subtitle}</div>
     </div>
-    <div className="w-20 f6 fw5 light-gray mr2">{period}</div>
+    <div
+      className="w-20 f6 fw5 light-gray mr2"
+      dangerouslySetInnerHTML={{ __html: period }}
+    />
   </div>
 );
 
-const HomePage = ({ projects, work, education }) => {
+const HomePage = ({ projects, work, education, communities }) => {
   const [showTeams, setShowTeams] = useState(true);
   const [showProjects, setShowProjects] = useState(true);
   const [showWork, setShowWork] = useState(false);
+  const [showCommunities, setShowCommunities] = useState(false);
   const filteredWork = showWork ? work : work.slice(0, 3);
+  const filteredCommunities = showCommunities
+    ? communities
+    : communities.slice(0, 2);
   return (
     <div className="helvetica relative bg-near-white near-black pv5">
       <Meta
@@ -47,8 +55,21 @@ const HomePage = ({ projects, work, education }) => {
       <div className="mw8 center mb5">
         <h3 className="prime purple f4 fw4">Projects</h3>
       </div>
-      <div className={cx("mw8 center fw4", { mb5: showWork })}>
-        <h3 className="prime purple f4 fw4 pb1">Work Experience</h3>
+      <div className="mw8 center fw4 mb5">
+        <div className="flex items-center mb3 pb1 justify-between">
+          <h3 className="prime purple f4 fw4 mv0">Work Experience</h3>
+          {!showWork && (
+            <div className="flex justify-center">
+              <button
+                onClick={() => setShowWork(true)}
+                className="ba bg-transparent b--moon-gray gray fw5 br2 ph2 lh-solid pv1 pointer"
+                style={{ fontSize: "12px" }}
+              >
+                Show More
+              </button>
+            </div>
+          )}
+        </div>
         <div className="mt3 br3 bg-white shadow">
           {filteredWork.map((item, key) => (
             <RowItem
@@ -61,16 +82,34 @@ const HomePage = ({ projects, work, education }) => {
             />
           ))}
         </div>
-        {!showWork && (
-          <div className="mt3 flex justify-center">
-            <button
-              onClick={() => setShowWork(true)}
-              className="ba b--moon-gray purple f6 fw5 br2 ph3 pv1 pointer"
-            >
-              Show More
-            </button>
-          </div>
-        )}
+      </div>
+      <div className="mw8 center fw4 mb5">
+        <div className="flex items-center mb3 pb1 justify-between mt4">
+          <h3 className="prime purple f4 fw4 mv0">Communities</h3>
+          {!showCommunities && (
+            <div className="flex justify-center">
+              <button
+                onClick={() => setShowCommunities(true)}
+                className="ba bg-transparent b--moon-gray gray fw5 br2 ph2 lh-solid pv1 pointer"
+                style={{ fontSize: "12px" }}
+              >
+                Show More
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="mt3 br3 bg-white shadow">
+          {filteredCommunities.map((item, key) => (
+            <RowItem
+              key={key}
+              logo={item.logo}
+              title={item.role}
+              subtitle={item.company}
+              description={item.description}
+              period={item.period}
+            />
+          ))}
+        </div>
       </div>
       <div className="mw8 center mb5">
         <h3 className="prime purple f4 fw4">Education</h3>
@@ -86,16 +125,16 @@ const HomePage = ({ projects, work, education }) => {
           ))}
         </div>
       </div>
-      <div className="mw8 center mb5">
-        <h3 className="prime purple f4 fw4">Contact</h3>
-        <p className="mt2">
+      <div className="mw8 center mb4">
+        <h3 className="prime purple f4 fw4 mb2">Contact</h3>
+        <p className="mt2 dark-gray">
           If you are intested in working together, send me a message or{" "}
           <Anchor href="mailto:nikolazaris@gmail.com" className="color-inherit">
             email me
           </Anchor>
           .
         </p>
-        <div className="flex justify-center center mt4">
+        <div className="flex purple justify-center center mt4 pt3">
           <Anchor className="color-inherit" href="https://github.com/nlaz">
             GitHub
           </Anchor>
@@ -123,7 +162,7 @@ HomePage.getInitialProps = async () => {
   return {
     projects: [],
     work: work,
-    communities: [],
+    communities: communities,
     education: education,
   };
 };
